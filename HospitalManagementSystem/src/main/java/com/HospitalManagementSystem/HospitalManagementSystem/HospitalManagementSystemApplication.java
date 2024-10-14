3c20ee7eba8ff091dd47aef09428db2b24695215
It22899224J <it22899224@my.sliit.lk>
2024-10-14 11:41:05 +0530

    properties update

--- HospitalManagementSystem/src/main/java/com/HospitalManagementSystem/HospitalManagementSystem/HospitalManagementSystemApplication.java
+++ HospitalManagementSystem/src/main/java/com/HospitalManagementSystem/HospitalManagementSystem/HospitalManagementSystemApplication.java
@@ -2,8 +2,13 @@ package com.HospitalManagementSystem.HospitalManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+//@EnableCaching
+//@EnableAsync
+////@EnableRetry
 public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
